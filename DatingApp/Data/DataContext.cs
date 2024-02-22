977376364c398161de059ffc6253ac8a4617606b
Mushfique <mushfique3214@gmail.com>
2024-02-22 21:08:53 +0600

    Add project files.

---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+	public class DataContext : DbContext
+	{
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
